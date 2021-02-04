8ebc0363b70893113221f5286a459173dac773d4
ddrrqq <ddrrqq@126.com>
2021-02-04 18:44:51 +0800

    商品列表页面实现。

---
+++ components/my-goods-item/my-goods-item.vue
@@ -0,0 +1,71 @@
+<template>
+	<view class="goods-item" @click="goodsClickHandler">
+	    <view class="image-box">
+	        <image :src="goods.goods_small_logo || '/static/no-image.png'"></image>
+	    </view>
+	    <view class="info-box">
+	        <view class="name">
+	            {{goods.goods_name}}
+	        </view>
+	        <view class="price">
+	            ￥{{goods.goods_price | money}}
+	        </view>
+	    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+        props:{
+            goods: {
+                type: Object,
+                default: {}
+            }
+        },
+		data() {
+			return {
+				
+			};
+		},
+        filters: {
+            money(value){
+                return value.toFixed(2)
+            }
+        },
+        methods: {
+            goodsClickHandler(){
+                this.$emit('click')
+            }
+        }
+	}
+</script>
+
+<style lang="less">
+    .goods-item {
+        display: flex;
+        box-sizing: border-box;
+        padding: 10px;
+        border-bottom: 1px solid #eee;
+        .image-box {
+            image {
+                width: 100px;
+                height: 100px;
+            }
+        }
+        
+        .info-box {
+            margin-left: 10px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            
+            .name {
+                font-size: 13px;
+            }
+            
+            .price {
+                color: #f00;
+            }
+        }
+    }
+</style>
--- sub/goods_list/goods_list.vue
+++ sub/goods_list/goods_list.vue
@@ -3,19 +3,10 @@
 		<!-- 商品列表区域 -->
         <view class="goods-list">
             <!-- 没每一件商品 -->
-            <view class="goods-item" v-for="(i, idx) in goodsList" :key="idx">
-                <view class="image-box">
-                    <image :src="i.goods_small_logo" mode=""></image>
-                </view>
-                <view class="info-box">
-                    <view class="name">
-                        {{i.goods_name}}
-                    </view>
-                    <view class="price">
-                        ￥{{i.goods_price}}
-                    </view>
-                </view>
-            </view>
+            <my-goods-item v-for="(i, idx) in goodsList" :key="idx" :goods="i" @click="goGoodsDetail(i.goods_id)"></my-goods-item>
+        </view>
+        <view class="full-tips" v-if="isFull">
+            我是有底线的，没有更多了~
         </view>
 	</view>
 </template>
@@ -30,26 +21,62 @@
                 pagesize: 10,
                 
                 goodsList: [],
-                total: 0
+                total: 1,
+                
+                isLoading: false
 			};
 		},
+        computed:{
+            isFull(){
+                return this.goodsList.length >= this.total;
+            }
+        },
         onLoad({ cid = '', query = '' }){
             this.cid = cid;
             this.query = query;
             
             this.getGoodsList()
         },
+        onReachBottom(){
+            if (this.isLoading || this.isFull) return;
+            // 页码值自增加一。
+            this.pagenum = this.pagenum + 1;
+            
+            // 获取新的列表。
+            this.getGoodsList();
+        },
+        onPullDownRefresh(){
+            // 重置数据。
+            this.pagenum = 1;
+            this.goodsList = [];
+            this.isLoading = false;
+            
+            // 重新获取商品列表。
+            this.getGoodsList(true);
+        },
         methods: {
-            async getGoodsList(){
+            goGoodsDetail(id){
+                uni.navigateTo({
+                    url: '/sub/goods_detail/goods_detail?id=' + id
+                })
+            },
+            async getGoodsList(isRefresh = false){
                 // 拿到基本数据。
                 const { query, cid, pagenum, pagesize } = this;
+                this.isLoading = true;
                 
-                // 发送请求获取数据。
-                const {data: res} = await uni.$http.get('/goods/search', { query, cid, pagenum, pagesize });
-                
-                if (res.meta.status === 200) {
-                    this.goodsList = res.message.goods;
-                    this.total = res.message.total;
+                // try ... catch ... finally
+                try {
+                    // 发送请求获取数据。
+                    const {data: res} = await uni.$http.get('/goods/search', { query, cid, pagenum, pagesize });
+                    
+                    if (res.meta.status === 200) {
+                        this.goodsList = this.goodsList.concat(res.message.goods);
+                        this.total = res.message.total;
+                    }
+                } finally {
+                    this.isLoading = false;
+                    isRefresh && uni.stopPullDownRefresh();
                 }
             }
         }
@@ -57,5 +84,11 @@
 </script>
 
 <style lang="less">
-
+    .full-tips {
+        text-align: center;
+        line-height: 28px;
+        background-color: #f1f1f1;
+        color: #ccc;
+        font-size: 12px;
+    }
 </style>

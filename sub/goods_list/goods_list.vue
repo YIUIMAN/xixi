7a8100e37ea88b99429c396a357130794eed98d9
ddrrqq <ddrrqq@126.com>
2021-02-04 16:10:39 +0800

    商品列表。

--- sub/goods_list/goods_list.vue
+++ sub/goods_list/goods_list.vue
@@ -1,6 +1,22 @@
 <template>
 	<view>
-		
+		<!-- 商品列表区域 -->
+        <view class="goods-list">
+            <!-- 没每一件商品 -->
+            <view class="goods-item" v-for="(i, idx) in goodsList" :key="idx">
+                <view class="image-box">
+                    <image :src="i.goods_small_logo" mode=""></image>
+                </view>
+                <view class="info-box">
+                    <view class="name">
+                        {{i.goods_name}}
+                    </view>
+                    <view class="price">
+                        ￥{{i.goods_price}}
+                    </view>
+                </view>
+            </view>
+        </view>
 	</view>
 </template>
 
@@ -8,9 +24,35 @@
 	export default {
 		data() {
 			return {
-				
+				query: '',
+                cid: '',
+                pagenum: 1,
+                pagesize: 10,
+                
+                goodsList: [],
+                total: 0
 			};
-		}
+		},
+        onLoad({ cid = '', query = '' }){
+            this.cid = cid;
+            this.query = query;
+            
+            this.getGoodsList()
+        },
+        methods: {
+            async getGoodsList(){
+                // 拿到基本数据。
+                const { query, cid, pagenum, pagesize } = this;
+                
+                // 发送请求获取数据。
+                const {data: res} = await uni.$http.get('/goods/search', { query, cid, pagenum, pagesize });
+                
+                if (res.meta.status === 200) {
+                    this.goodsList = res.message.goods;
+                    this.total = res.message.total;
+                }
+            }
+        }
 	}
 </script>
 
